b5d56c2e7f06a973fc21211513f65a7b5a50ff87
Mayar Mahrous <mayar.mahrous@AssetDev.com>
2021-12-30 19:33:24 +0200

    ContactsApp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 import ListContacts from './ListContacts';
 import * as ContactsAPI from './ContactsAPI';
 import CreateContact from './CreateContact';
